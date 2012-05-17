1d6f73defdfeb88649ee11b15b33df998a28734e
Elmar PD3EM <info@pd3em.com>
2012-05-17 20:24:06 +0200

    added temperature compensation

--- PD3EM_Tracker_v0_5/readMS5607.ino
+++ PD3EM_Tracker_v0_5/readMS5607.ino
@@ -1,27 +1,33 @@
-uint32_t D1 = 0;
-uint32_t D2 = 0;
-int32_t dT = 0;
-int32_t TEMP = 0;
-int64_t OFF = 0;
-int64_t SENS = 0;
-int32_t P = 0;
-uint16_t C[7];
-int32_t T2 = 0;
-int64_t OFF2 = 0;
-int64_t SENS2 = 0;
-
 void getMS5607()
 {
+   int32_t T2 = 0;
+   int64_t OFF2 = 0;
+   int64_t SENS2 = 0;
     D1 = getVal(ADDRESS, 0x48); // Pressure raw
     D2 = getVal(ADDRESS, 0x58);// Temperature raw
   
     dT = (D2 - C[4] * 256);
-    TEMP = 2000 + dT * C[5]/8388608;
+    TEMP = 2000 + dT * C[5]/8388608LL;
     OFF  = C[1] * 131072LL + (dT *C[3])/64; // was /128
     SENS = C[0] * 65536LL + (C[2] * dT)/128; // was /256
-    P = (D1 * SENS/2097152 - OFF)/32768;
+    
+    if ( TEMP < 2000){
+      T2 = pow(dT,2) / 2147483648LL;
+      OFF2 = 61 * pow((TEMP-2000),2) / 16;
+      SENS2 = 2 * pow((TEMP-2000),2);
+    
+    if (TEMP < -1500){
+      OFF2 = OFF2 + 15 * pow((TEMP +1500),2);
+      SENS2 = SENS2 + 8 * pow((TEMP +1500),2);
+    }
+    }
+    TEMP = TEMP - T2;
+    OFF = OFF - OFF2;
+    SENS = SENS - SENS2;
+
+    P = (D1 * SENS/2097152LL - OFF)/32768LL;
     temp1 = TEMP/100.00;
-    press1 = P/100.0 + 10.0;
+    press1 = P/100.0;
 }
 
 long getVal(int address, byte code)
